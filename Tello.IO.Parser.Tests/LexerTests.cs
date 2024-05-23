5b037b7c2d717deedddeb17ea1554c3c720d8d66
Mark Lauter <mark@marklauter.com>
2024-05-22 21:25:12 -0400

    parser supports dotnet 6 and 7

--- Tello.IO.Parser.Tests/LexerTests.cs
+++ Tello.IO.Parser.Tests/LexerTests.cs
@@ -61,40 +61,40 @@ public class LexerTests(Lexer lexer)
     }
 
     [Theory]
-    [InlineData("up 100", new[] { TokenId.MoveUp, TokenId.IntegerLiteral })]
-    [InlineData("down 50", new[] { TokenId.MoveDown, TokenId.IntegerLiteral })]
-    [InlineData("left 60", new[] { TokenId.MoveLeft, TokenId.IntegerLiteral })]
-    [InlineData("right 60", new[] { TokenId.MoveRight, TokenId.IntegerLiteral })]
-    [InlineData("forward 60", new[] { TokenId.MoveForward, TokenId.IntegerLiteral })]
-    [InlineData("back 60", new[] { TokenId.MoveBack, TokenId.IntegerLiteral })]
-    [InlineData("cw 180", new[] { TokenId.RotateClockwise, TokenId.IntegerLiteral })]
-    [InlineData("ccw 360", new[] { TokenId.RotateCounterClockwise, TokenId.IntegerLiteral })]
-    [InlineData("flip l", new[] { TokenId.Flip, TokenId.DirectionLeft })]
-    [InlineData("flip r", new[] { TokenId.Flip, TokenId.DirectionRight })]
-    [InlineData("flip b", new[] { TokenId.Flip, TokenId.DirectionBack })]
-    [InlineData("flip f", new[] { TokenId.Flip, TokenId.DirectionFront })]
-    [InlineData("go 0 50 50 25", new[] { TokenId.MoveToPosition, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral })]
-    [InlineData("curve 0 50 50 0 100 100 25", new[] { TokenId.Curve, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral })]
-    [InlineData("speed 20", new[] { TokenId.WriteSpeed, TokenId.IntegerLiteral })]
-    [InlineData("rc 0 50 -20 -9", new[] { TokenId.WriteRemoteControl, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral })]
-    [InlineData("wifi ssidvalue password", new[] { TokenId.WriteWifi, TokenId.Identifier, TokenId.Identifier })]
-    [InlineData("ap ssidvalue password", new[] { TokenId.WriteAccessPoint, TokenId.Identifier, TokenId.Identifier })]
-    [InlineData("wifisetchannel 10", new[] { TokenId.WriteWifiChannel, TokenId.IntegerLiteral })]
-    [InlineData("port 8889 8899", new[] { TokenId.WritePort, TokenId.IntegerLiteral, TokenId.IntegerLiteral })]
-    [InlineData("setfps low", new[] { TokenId.WriteFramesPerSecond, TokenId.Identifier })]
-    [InlineData("setfps middle", new[] { TokenId.WriteFramesPerSecond, TokenId.Identifier })]
-    [InlineData("setfps high", new[] { TokenId.WriteFramesPerSecond, TokenId.Identifier })]
-    [InlineData("setbitrate 5", new[] { TokenId.WriteBitrate, TokenId.IntegerLiteral })]
-    [InlineData("setresolution low", new[] { TokenId.WriteResolution, TokenId.Identifier })]
-    [InlineData("setresolution high", new[] { TokenId.WriteResolution, TokenId.Identifier })]
-    public void NextMatch_Returns_Expected_Tokens(string input, TokenId[] expectedTokens)
+    [InlineData("up 100", TokenId.MoveUp, TokenId.IntegerLiteral)]
+    [InlineData("down 50", TokenId.MoveDown, TokenId.IntegerLiteral)]
+    [InlineData("left 60", TokenId.MoveLeft, TokenId.IntegerLiteral)]
+    [InlineData("right 60", TokenId.MoveRight, TokenId.IntegerLiteral)]
+    [InlineData("forward 60", TokenId.MoveForward, TokenId.IntegerLiteral)]
+    [InlineData("back 60", TokenId.MoveBack, TokenId.IntegerLiteral)]
+    [InlineData("cw 180", TokenId.RotateClockwise, TokenId.IntegerLiteral)]
+    [InlineData("ccw 360", TokenId.RotateCounterClockwise, TokenId.IntegerLiteral)]
+    [InlineData("flip l", TokenId.Flip, TokenId.DirectionLeft)]
+    [InlineData("flip r", TokenId.Flip, TokenId.DirectionRight)]
+    [InlineData("flip b", TokenId.Flip, TokenId.DirectionBack)]
+    [InlineData("flip f", TokenId.Flip, TokenId.DirectionFront)]
+    [InlineData("go 0 50 50 25", TokenId.MoveToPosition, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral)]
+    [InlineData("curve 0 50 50 0 100 100 25", TokenId.Curve, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral)]
+    [InlineData("speed 20", TokenId.WriteSpeed, TokenId.IntegerLiteral)]
+    [InlineData("rc 0 50 -20 -9", TokenId.WriteRemoteControl, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral, TokenId.IntegerLiteral)]
+    [InlineData("wifi ssidvalue password", TokenId.WriteWifi, TokenId.Identifier, TokenId.Identifier)]
+    [InlineData("ap ssidvalue password", TokenId.WriteAccessPoint, TokenId.Identifier, TokenId.Identifier)]
+    [InlineData("wifisetchannel 10", TokenId.WriteWifiChannel, TokenId.IntegerLiteral)]
+    [InlineData("port 8889 8899", TokenId.WritePort, TokenId.IntegerLiteral, TokenId.IntegerLiteral)]
+    [InlineData("setfps low", TokenId.WriteFramesPerSecond, TokenId.Identifier)]
+    [InlineData("setfps middle", TokenId.WriteFramesPerSecond, TokenId.Identifier)]
+    [InlineData("setfps high", TokenId.WriteFramesPerSecond, TokenId.Identifier)]
+    [InlineData("setbitrate 5", TokenId.WriteBitrate, TokenId.IntegerLiteral)]
+    [InlineData("setresolution low", TokenId.WriteResolution, TokenId.Identifier)]
+    [InlineData("setresolution high", TokenId.WriteResolution, TokenId.Identifier)]
+    public void NextMatch_Returns_Expected_Tokens(string input, params TokenId[] expected)
     {
         var i = 0;
         var result = lexer.NextMatch(input);
         while (result.Symbol.IsMatch)
         {
             Assert.True(result.Symbol.IsMatch);
-            Assert.Equal((uint)expectedTokens[i++], result.Symbol.TokenId);
+            Assert.Equal((uint)expected[i++], result.Symbol.TokenId);
             result = lexer.NextMatch(result);
         }
     }
